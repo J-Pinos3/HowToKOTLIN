04457e5ee7329fa5df7ed3481d0cee53fc329a54
J-Pinos3 <josloco665@gmail.com>
2023-04-30 20:11:50 -0500

    Variables y tipos de datos

--- HolaMundo/app/src/main/java/com/example/holamundo/Main.kt
+++ HolaMundo/app/src/main/java/com/example/holamundo/Main.kt
@@ -8,7 +8,22 @@ fun main(){
     val edad: Int = 10
     //edad= 11 ERROR!! no se puede reasignar
     println("La edad es: " + "$edad")//imprime un salto de línea
-    print("La edad es: " + "$edad")
+    print("La edad es: " + "$edad" + "\n\n\n")
 
+    var valor: Int = 45
+    val num_byte:Byte = 2;
+    val num_entero:Int = 15;
+    val num_float: Float = 15.63f;
+    val num_double: Double = 4.52;
+    val cadena: String = "Hola, soy una cadena de texto"
 
+    println("\'Variable\' valor: " + valor)
+    println("Valor byte: " + num_byte);
+    println("Valor entero: " + "$num_entero");
+    println("Valor float: " + num_float);
+    println("Valor double: " + "$num_double");
+    println("Valor string: " + cadena)
+
+    valor += 10
+    println("\'Variable\' valor: $valor")
 }
