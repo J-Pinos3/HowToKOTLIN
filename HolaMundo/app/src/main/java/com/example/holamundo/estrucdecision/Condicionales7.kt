708e017c8e1a3d01288f0eb6889c512f2d3bcc84
J-Pinos3 <josloco665@gmail.com>
2023-05-07 21:27:06 -0500

    Ejercicios con when, if-else

--- HolaMundo/app/src/main/java/com/example/holamundo/estrucdecision/Condicionales7.kt
+++ HolaMundo/app/src/main/java/com/example/holamundo/estrucdecision/Condicionales7.kt
@@ -13,10 +13,20 @@ fun notaPalabras(nota: Int): String{
 
     var estado:String = ""
     when(nota){
+        /*
         0,1,2,3 -> estado = "Reprobado"
         4,5,6-> estado = "Supletorio"
         7,8,9,10 -> estado = "Aprobado"
         else -> estado = "Nota errónea"
+        */
+
+        /*is-condition y existe in-condition*/
+        /*is Int -> CÓDIGO*/
+        /*!is   !in  operador de negación*/
+
+        in 0..4 -> estado = "Reprobado"
+        in 5..10 -> estado = "Aprobado"
+        else -> estado = "Nota incorrecta"
     }
 
     return estado
