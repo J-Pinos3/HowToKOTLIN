7771800fae82a35bd6cf67331758c22e6961ca05
J-Pinos3 <josloco665@gmail.com>
2023-07-22 07:18:03 -0500

    Inicio APP TODO, creación de colores, themes y de la activity

--- HolaMundo/app/src/main/java/com/example/holamundo/MenuActivity.kt
+++ HolaMundo/app/src/main/java/com/example/holamundo/MenuActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.widget.Button
 import com.example.holamundo.firstapp.PrimeraAppActivity
 import com.example.holamundo.imccalculator.ImcCalculatorActivity
+import com.example.holamundo.todoapp.TodoMainActivity
 
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,6 +19,14 @@ class MenuActivity : AppCompatActivity() {
         val btnAppIMC = findViewById<Button>(R.id.btnAppIMC)
         btnAppIMC.setOnClickListener { navega_AppIMC() }
 
+        val btnAppToDo = findViewById<Button>(R.id.btnAppTODO)
+        btnAppToDo.setOnClickListener { navega_AppTodo() }
+
+    }
+
+    private fun navega_AppTodo() {
+        val intent = Intent(this, TodoMainActivity::class.java)
+        startActivity(intent)
     }
 
 
---
+++ HolaMundo/app/src/main/java/com/example/holamundo/todoapp/TodoMainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.holamundo.todoapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.holamundo.R
+
+class TodoMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_todo_main)
+    }
+}
