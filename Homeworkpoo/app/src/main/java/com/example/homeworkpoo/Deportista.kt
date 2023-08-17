f65ed372322009aa5e769b92e2fe1a58834d218f
J-Pinos3 <josloco665@gmail.com>
2023-08-17 10:05:54 -0500

    Main Function, Runner class & abstract Deportist player

---
+++ Homeworkpoo/app/src/main/java/com/example/homeworkpoo/Deportista.kt
@@ -0,0 +1,32 @@
+package com.example.homeworkpoo
+
+abstract class Deportista(private var name: String = "",
+                          private  var estatura: Float = 0f,
+                          private  var peso: Float = 0f,
+                          private  var edad: Int = 0 ){
+
+
+        fun Deportista(name:String, estatura:Float, peso: Float, edad:Int){
+                this.name = name
+                this.estatura = estatura
+                this.peso = peso
+                this.edad = edad
+        }
+
+        fun getName(): String = name
+
+        fun getHeight(): Float = this.estatura
+
+        fun getWeight(): Float{
+            return this.peso
+        }
+
+        fun getAge(): Int { return edad }
+
+        open fun presentarse(): String{
+
+            return "Hola, yo me llamo ${getName()}, tengo ${getAge()} años.\n" +
+                    "Físicamente mi estatura es ${getHeight()} y peso ${getWeight()} Kg."
+        }
+
+}
---
+++ Homeworkpoo/app/src/main/java/com/example/homeworkpoo/Runner.kt
@@ -0,0 +1,41 @@
+package com.example.homeworkpoo
+
+class Runner(var velocidad: Float = 20.0f, name: String = "", estatura: Float = 0f, peso: Float = 0f, edad: Int = 0):
+    Deportista(name, estatura, peso, edad) {
+
+    var tipoRunner: String = ""
+    var arrayCategories:Array<String> = arrayOf(
+        "100 METROS LISOS","400 METROS CON OBSTÁCULOS",
+        "MARATÓN", "FONDO", "RELEVOS"
+
+    )
+
+    fun Runner(n: String, est:Float, pes:Float, age:Int, veloci: Float, tR: String){
+        velocidad = veloci
+        tipoRunner = tR
+        super.Deportista(n, est, pes, age)
+    }
+
+
+    fun seleccionaDisciplina(posicion:Int): String{
+        return arrayCategories[posicion]
+    }
+
+    fun mostrarCategorias(){
+        println("Las categorías para corredores son: \n")
+        var i = 0
+        for(k in arrayCategories){
+            println("$k (${++i}) ")
+        }
+    }
+
+    override
+    fun presentarse():String{
+        return "${super.presentarse()}\n" +
+                "Mi categoría es: ${tipoRunner}\n\n."
+    }
+
+    //debe contener estilo(100m lisos, 400m con obstaculos, maratón...) y velocidad
+}
+
+
