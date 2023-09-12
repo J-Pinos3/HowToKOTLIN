af3a9c92d599507768c6654cbaf09dbb714771f6
J-Pinos3 <josloco665@gmail.com>
2023-09-12 08:36:40 -0500

    Edit text, edit text con (addTextChangedListener)  íconos y estilo

--- testPeletas/app/src/main/java/com/example/testpeletas/MainActivity.kt
+++ testPeletas/app/src/main/java/com/example/testpeletas/MainActivity.kt
@@ -4,8 +4,10 @@ import android.graphics.Color
 import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
 import com.example.testpeletas.R
 
 class MainActivity : AppCompatActivity() {
@@ -26,13 +28,25 @@ class MainActivity : AppCompatActivity() {
 //        textViewMensaje.setTextColor(Color.BLUE)
 //        textViewMensaje.text = "Texto cambiado desde código"
 //        textViewMensaje.setTypeface(Typeface.MONOSPACE, Typeface.BOLD)
-
-
         textViewMensaje.setOnClickListener {
             Toast.makeText(this, "Nuevo Texto On Click", Toast.LENGTH_SHORT).show()
             textViewMensaje.setTextColor(Color.RED)
         }
 
 
-    }
+        var etTextNombre = findViewById<EditText>(R.id.etTextNombre)
+        //cada vez que se cambie el contenido, se ejecutará esto
+        etTextNombre.addTextChangedListener{
+            if(etTextNombre.text.length == 0) etTextNombre.setError("Campo vacío")
+
+        }
+        etTextNombre.setSelection(3)
+        var inicio = etTextNombre.selectionStart
+        var fin = etTextNombre.selectionEnd
+        etTextNombre.selectAll()
+
+
+
+
+    }//FIN DE ON CREATE
 }
