e111287513d6dfac5a391a32004ba432c773445c
J-Pinos3 <josloco665@gmail.com>
2023-09-13 08:37:59 -0500

    EditText's ordenados, seleccion en edittext, autocompleteTextView(Multi), array de strings

--- testPeletas/app/src/main/java/com/example/testpeletas/MainActivity.kt
+++ testPeletas/app/src/main/java/com/example/testpeletas/MainActivity.kt
@@ -4,7 +4,10 @@ import android.graphics.Color
 import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import android.widget.EditText
+import android.widget.MultiAutoCompleteTextView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.core.widget.addTextChangedListener
@@ -33,20 +36,50 @@ class MainActivity : AppCompatActivity() {
             textViewMensaje.setTextColor(Color.RED)
         }
 
-
+        /*
         var etTextNombre = findViewById<EditText>(R.id.etTextNombre)
         //cada vez que se cambie el contenido, se ejecutará esto
         etTextNombre.addTextChangedListener{
             if(etTextNombre.text.length == 0) etTextNombre.setError("Campo vacío")
 
         }
+        etTextNombre.setSelection(4)
+        var inicio = etTextNombre.selectionStart
+        var fin = etTextNombre.selectionEnd
+        etTextNombre.setSelection(inicio, fin)
+        etTextNombre.selectAll()
+        */
+        var etTextNombre = findViewById<EditText>(R.id.etTextNombre)
+        etTextNombre.addTextChangedListener {
+            if (etTextNombre.text.length == 0) etTextNombre.setError("Campo vacío")
+        }
         etTextNombre.setSelection(3)
         var inicio = etTextNombre.selectionStart
         var fin = etTextNombre.selectionEnd
+
+        etTextNombre.setSelection(inicio, fin)
         etTextNombre.selectAll()
 
 
 
 
+        var autoCompleteTextView = findViewById<AutoCompleteTextView>(R.id.autoCompleteTextView)
+        var paises: Array<String> = resources.getStringArray(R.array.countries_array)
+
+        var adapter: ArrayAdapter<String> = ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line,paises)
+
+        autoCompleteTextView.setAdapter(adapter)
+
+
+
+        var multiAutoCompleteTextView = findViewById<MultiAutoCompleteTextView>(R.id.multiAutoCompleteTextView)
+        multiAutoCompleteTextView.setAdapter(adapter)
+        multiAutoCompleteTextView.setTokenizer(MultiAutoCompleteTextView.CommaTokenizer())
+
     }//FIN DE ON CREATE
 }
+
+
+
+
+
